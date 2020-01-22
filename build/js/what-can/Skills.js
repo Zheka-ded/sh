414b63b990e9f9a83bb552909417f2f54519bfd2
Zheka-ded <11vitamin11@rambler.ru>
2020-01-22 21:59:15 +0200

    work check

--- build/js/what-can/Skills.js
+++ build/js/what-can/Skills.js
@@ -1,27 +1,27 @@
 class Skills {
-    constructor (icon, title, subtitle){
+    constructor (blockName, icon, title, subtitle){
+        this.blockName = blockName;
         this.icon = icon;
         this.title = title;
         this.subtitle = subtitle;
-        // this.userDescription = userDescription;
     }
     buildSkills (out) {
         let items = document.createElement('div');
-        items.classList.add('what-can__item');
+        items.classList.add(`${this.blockName}__item`);
         out.append(items);
 
         let svgWrap = document.createElement('div');
-        svgWrap.classList.add('hexagon', 'what-can__svg-wrap');
+        svgWrap.classList.add(`${this.blockName}__svg-wrap`);
         svgWrap.innerHTML = this.icon;
         items.append(svgWrap);
 
         let titleText = document.createElement('p');
-        titleText.classList.add('what-can__title');
+        titleText.classList.add(`${this.blockName}__title`);
         titleText.textContent = this.title;
         items.append(titleText);
 
         let subtitleText = document.createElement('p');
-        subtitleText.classList.add('what-can__title');
+        subtitleText.classList.add(`${this.blockName}__subtitle`);
         subtitleText.textContent = this.subtitle;
         items.append(subtitleText);
     }

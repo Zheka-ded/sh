9eeeeec0a78d45fb039f2fd41319e0214b5a3efd
Zheka-ded <11vitamin11@rambler.ru>
2020-01-22 07:09:18 +0200

    svg icon in items and section 'what-can'

--- src/blocks/what-can/Skills.js
+++ src/blocks/what-can/Skills.js
@@ -1,11 +1,28 @@
 class Skills {
     constructor (icon, title, subtitle){
-        this.src = icon;
+        this.icon = icon;
         this.title = title;
         this.subtitle = subtitle;
-        this.userDescription = userDescription;
+        // this.userDescription = userDescription;
     }
     buildSkills (out) {
-        
+        let items = document.createElement('div');
+        items.classList.add('what-can__item');
+        out.append(items);
+
+        let svgWrap = document.createElement('div');
+        svgWrap.classList.add('hexagon', 'what-can__svg-wrap');
+        svgWrap.innerHTML = this.icon;
+        items.append(svgWrap);
+
+        let titleText = document.createElement('p');
+        titleText.classList.add('what-can__title');
+        titleText.textContent = this.title;
+        items.append(titleText);
+
+        let subtitleText = document.createElement('p');
+        subtitleText.classList.add('what-can__title');
+        subtitleText.textContent = this.subtitle;
+        items.append(subtitleText);
     }
 }

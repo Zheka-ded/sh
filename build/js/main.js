d792f510a18691d3c2fcb3cf816b0446b8c08cdf
Zheka-ded <11vitamin11@rambler.ru>
2020-01-31 08:04:02 +0200

    styled form

--- build/js/main.js
+++ build/js/main.js
@@ -14,19 +14,26 @@ const FORM_USER_PHONE = document.querySelector(`[name="${FORM_USER_PHONE_CSS}"]`
 const FORM_SEND_CSS = 'send_form';
 const FORM_SEND = document.querySelector(`[name="${FORM_SEND_CSS}"]`);
 
+let r = /^[\w\.\d-_]+@[\w\.\d-_]+\.\w{2,4}$/i;
 
-FORM_SEND.onclick = function (event) {
-    event.preventDefault()
-    console.log(FORM_USER_NAME.value);
-    console.log(FORM_USER_EMAIL.value);
-    console.log(FORM_USER_PHONE.value);
-    if (FORM_USER_NAME.value != '' && FORM_USER_EMAIL.value != '' && FORM_USER_PHONE.value != '') {
-        sendFormFunc()
+FORM_SEND.onclick = function checkFormFunc (event) {
+    event.preventDefault();
+    
+    if (FORM_USER_NAME.value.trim() != '' && FORM_USER_EMAIL.value.trim() != '' && FORM_USER_PHONE.value.trim() != '') {
+        if (!r.test(FORM_USER_EMAIL.value.trim())) {
+            alert('Веден не верный адрес почты');
+        } else{
+            sendFormFunc()
+        }
     } else {
         alert('Заполните все поля');
     }
 }
 
+// FORM_SEND.onclick = checkFormFunc;
+// // FORM_SEND.ontouchstart = checkFormFunc;
+// FORM_SEND.addEventListener('touchstart', checkFormFunc);
+// FORM_SEND.addEventListener('click', checkFormFunc);
 
 function sendFormFunc() {
     fetch('mail.php', {
@@ -34,7 +41,7 @@ function sendFormFunc() {
         'headers': {
             'Content-Type': 'application/x-www-form-urlencoded',
         },
-        'body': `user_name=${FORM_USER_NAME.value}&user_email=${FORM_USER_EMAIL.value}&user_phone=${FORM_USER_PHONE.value}`
+        'body': `user_name=${FORM_USER_NAME.value.trim()}&user_email=${FORM_USER_EMAIL.value.trim()}&user_phone=${FORM_USER_PHONE.value.trim()}`
     })
     .then(function () {window.location.href = 'thanks.html'})
 }
